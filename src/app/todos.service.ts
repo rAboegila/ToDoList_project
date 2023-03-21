27f9b94d957116de8f6a7ef78397466f1eb8f2c9
nadaezzat-99 <nadaezzat507@gmail.com>
2023-03-21 13:11:09 +0200

    Added Favourite, Compeleted, Deleted todos Compoment Functionality

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './lib';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
   todos: Todo[] = [
@@ -9,25 +9,25 @@ export class TodosService {
       id: 1,
       title: 'Do something nice for someone I care about',
       completed: false,
-      deleted:false,
-      favourite:false
+      deleted: false,
+      favourite: false,
     },
     {
       id: 2,
       title: 'Memorize the fifty states and their capitals',
       completed: false,
-      deleted:false,
-      favourite:false
+      deleted: false,
+      favourite: false,
     },
     {
       id: 3,
       title: 'Watch a classic movie',
       completed: false,
-      deleted:false,
-      favourite:false
+      deleted: false,
+      favourite: false,
     },
   ];
-  constructor() { }
+  constructor() {}
 
   addTodo(todoTitle: string) {
     if (todoTitle.trim().length > 0) {
@@ -48,27 +48,44 @@ export class TodosService {
   deleteTodo(todo: Todo) {
     todo.deleted = true;
     // this.todos.splice(this.todos.indexOf(todo), 1);
-
   }
 
   toggleTodoStatus(id: number) {
-     let selectedTodoIdx:number =this.todos.findIndex(todo=> todo.id === id);
-     this.todos[selectedTodoIdx].completed =!this.todos[selectedTodoIdx].completed;
-    console.log(this.todos);
-
-  
-    }
-
-  toggleTodoFavourite(id: number) {
-    let selectedTodoIdx:number =this.todos.findIndex(todo=> todo.id === id);
-    this.todos[selectedTodoIdx].favourite =!this.todos[selectedTodoIdx].favourite;
-    console.log(this.todos);
-    
+    let selectedTodoIdx: number = this.todos.findIndex(
+      (todo) => todo.id === id
+    );
+    this.todos[selectedTodoIdx].completed =
+      !this.todos[selectedTodoIdx].completed;
   }
 
-  getTodos():Todo[]{
-        let todos:Todo[] = this.todos.filter((todo:Todo) => !todo.deleted);
-        console.log(this.todos);
-        return todos;
+  toggleTodoFavourite(id: number) {
+    let selectedTodoIdx: number = this.todos.findIndex(
+      (todo) => todo.id === id
+    );
+    this.todos[selectedTodoIdx].favourite =
+      !this.todos[selectedTodoIdx].favourite;
+    console.log(this.todos);
+  }
+
+  getTodos(): Todo[] {
+    let todos: Todo[] = this.todos.filter((todo: Todo) => !todo.deleted);
+    console.log(this.todos);
+    return todos;
+  }
+
+  getFavouriteTodos(): Todo[] {
+    return this.todos.filter((todo) => todo.favourite);
+  }
+
+  getCompleteTodos(): Todo[] {
+    return this.todos.filter((todo) => todo.completed);
+  }
+
+  getdeletedTodos(): Todo[] {
+    return this.todos.filter((todo) => todo.deleted);
+  }
+
+  deleteTodoPermanent(todo: Todo) {
+    this.todos.splice(this.todos.indexOf(todo), 1);
   }
 }
